f3aed097c73c9dd6b8fabcb6269e820949dace5c
menhunging <menhunging@gmail.com>
2024-05-11 16:02:14 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,119 @@
+$(document).ready(function () {
+  if ($(".burger").length > 0) {
+    let menu = $(".header-mobile");
+    let burger = $(".burger");
+    let burgerСlose = $(".btn-close");
+
+    burger.on("click", function () {
+      if (menu.hasClass("opened")) {
+        closeMenu();
+      } else {
+        burger.addClass("opened");
+        menu.addClass("opened");
+
+        $(document).mouseup(function (e) {
+          if (
+            !menu.is(e.target) &&
+            menu.has(e.target).length === 0 &&
+            !burger.is(e.target)
+          ) {
+            closeMenu();
+          }
+        });
+      }
+    });
+
+    burgerСlose.on("click", function () {
+      closeMenu();
+    });
+
+    function closeMenu() {
+      burger.removeClass("opened");
+      menu.removeClass("opened");
+      $(document).off("mouseup");
+    }
+  }
+
+  if ($(".thisYear").length > 0) {
+    let date = new Date();
+    $(".thisYear").text(date.getFullYear());
+  }
+
+  if ($(".phone-input").length > 0) {
+    $(".phone-input").map(function () {
+      $(this).inputmask({
+        mask: "+7(999) 999-99-99",
+        placeholder: "*",
+        showMaskOnHover: false,
+        showMaskOnFocus: true,
+        clearIncomplete: true,
+      });
+    });
+  }
+
+  if ($(".modal").length > 0) {
+    MicroModal.init({
+      openTrigger: "data-modal",
+      disableScroll: true,
+      awaitOpenAnimation: true,
+      awaitCloseAnimation: true,
+
+      onShow: () => {
+        $("body").addClass("modal-open");
+      },
+
+      onClose: () => {
+        $("body").removeClass("modal-open");
+      },
+    });
+
+    $("[data-modal]").map(function () {
+      $(this).click((e) => {
+        e.preventDefault();
+        $("body").addClass("modal-open");
+      });
+    });
+
+    $("[data-micromodal-close]").map(function () {
+      $(this).click((e) => {
+        if ($(this).attr("data-modal")) {
+          setTimeout(() => {
+            $("body").addClass("modal-open");
+          }, 0.1);
+        }
+      });
+    });
+  }
+
+  if ($(".reviews-slider").length > 0) {
+    const swiper = new Swiper(".reviews-slider", {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      watchSlidesProgress: true,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      breakpoints: {
+        0: {
+          slidesPerView: 1.25,
+          spaceBetween: 16,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+      },
+    });
+  }
+});
+
+$(window).on("resize", function () {});
